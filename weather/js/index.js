a8ed399c9473e657db4427a37f2f75fe5eca1cff
Ray <lens1989@gmail.com>
2016-04-26 16:50:35 +0800

    edit weather

--- weather/js/index.js
+++ weather/js/index.js
@@ -1,19 +1,45 @@
 $(function(){
-	var now = new Date();
-	$('.j-date').html(new Date());
+	var now = moment().format('dddd, MMMM YYYY');
+	$('.j-date').html(now);
+	// getWeather();
+
+	$('.j-temp').on('click', function(){
+		$(this).find('span').toggle();
+	});
+});
+
+$('.j-list li').on('click', function(){
+	
+});
+
+function getWeather(){
 	$.getJSON('http://ipinfo.io', function(data){
 		var loc = data.loc.split(',');
 		$.ajax({
   			url: 'http://api.openweathermap.org/data/2.5/forecast?lat=' + loc[0] + '&lon=' + loc[1] +'&APPID=49bf450f5de5d3a0fe0be52bb321c4aa&units=metric',
   			success: function(data){
   				$('.j-city').html(data.city.name);
-  				var i = 0;
-  				while(now >= new Date(data.list[i].dt_txt)) {
-  					i++;
+  				var index = 0;
+  				for(var i = 0; i < data.list.length; i++) {
+  					index = i;
+  					if(moment().isBefore(data.list[i].dt_txt)) {
+  						break;
+  					}
   				}
-  				$('.j-temp').html(data.list[i].main.temp);
-  				console.log(data);
+  				var temp = data.list[index].main.temp;
+  				var tempHtml = '<span>' + Math.round(temp) + '°c</span><span style="display: none;">' + Math.round((temp * 9)/5 + 32) + '°f</span>';
+  				$('.j-temp').html(tempHtml);
+
+  				var list = '';
+				for(var j = 0; j < 5; j++) {
+					list += '<li data-t="' + data.list[3+8*j].main.temp + '"">' + moment().add(j, 'd').format('DD/MM') + '</li>';
+				}
+				$('.j-list').html(list);
   			}
   		});
 	});
-});
+}
+
+function setWeather(){
+
+}
