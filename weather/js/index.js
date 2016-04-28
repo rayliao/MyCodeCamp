c9051b02aa6feb989ca608cee7793be3a810706e
Ray <lens1989@gmail.com>
2016-04-28 12:04:22 +0800

    edit

--- weather/js/index.js
+++ weather/js/index.js
@@ -8,8 +8,9 @@ $(function(){
 	});
 });
 
-$('.j-list li').on('click', function(){
-	
+$(document).on('click', '.j-list > li', function(){
+  $(this).addClass('current').siblings().removeClass('current');
+  switchDay($(this).data('t'));
 });
 
 function getWeather(){
@@ -26,21 +27,33 @@ function getWeather(){
   						break;
   					}
   				}
-          console.log(data);
   				var temp = data.list[index].main.temp;
   				var tempHtml = '<span>' + Math.round(temp) + '°c</span><span style="display: none;">' + Math.round((temp * 9)/5 + 32) + '°f</span>';
   				$('.j-temp').html(tempHtml);
 
   				var list = '';
-				for(var j = 0; j < 5; j++) {
-					list += '<li data-t="' + data.list[3+8*j].main.temp + '""><i class="icon icon-rainy"></i><p>' + moment().add(j, 'd').format('DD/MM') + '</p></li>';
-				}
-				$('.j-list').html(list);
+  				for(var j = 0; j < 5; j++) {
+  					list += '<li class="' + (j==0 ? 'current': '') + '" data-t="' + data.list[3+8*j].main.temp + '""><i class="icon ' + getIcon(data.list[3+8*j].weather[0].main) + '"></i><p>' + moment().add(j, 'd').format('DD/MM') + '</p></li>';
+  				}
+				  $('.j-list').html(list);
   			}
   		});
 	});
 }
 
-function setWeather(){
+function switchDay(temp){
+  $('.j-temp').html('<span>' + Math.round(temp) + '°c</span><span style="display: none;">' + Math.round((temp * 9)/5 + 32) + '°f</span>')
+}
 
+function getIcon(type) {
+  switch(type.toLowerCase()){
+    case 'clouds':
+      return 'icon-clouds';
+    case 'rain':
+      return 'icon-rain';
+    case 'clear':
+      return 'icon-clear';
+    default:
+      return 'icon-clear';
+  }
 }
